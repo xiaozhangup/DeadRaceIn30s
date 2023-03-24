44f3753d07371f763b96de382e764184be6e0d22
Daniel Zhang <xiaozhangshiw@163.com>
2023-03-24 23:53:25 +0800

    移动等等的功能增强和界面美化

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,30 @@ plugins {
     id("java")
 }
 
-group = "org.example"
+group = "me.xiaozhangup.race"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
+tasks.jar.configure {
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    manifest.attributes["Main-Class"] = "me.xiaozhangup.race.CountdownGUI"
+    from(configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) })
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(16))
 }
 
 tasks.getByName<Test>("test") {
--- src/main/java/me/xiaozhangup/race/CountdownGUI.java
+++ src/main/java/me/xiaozhangup/race/CountdownGUI.java
@@ -1,22 +1,32 @@
 package me.xiaozhangup.race;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.RoundRectangle2D;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimerTask;
 
 public class CountdownGUI extends JFrame {
+    public static final Color CLOSE_BUTTON_TEXT_COLOR = new Color(246, 236, 217);
+    public static final Color CLOSE_BACKGROUND = new Color(235, 164, 157);
+    public static final Color BACKGROUND = new Color(243, 231, 206);
+    public static final Color FONT_COLOR = new Color(239, 159, 118);
+    public static final Color TIME_DONE_COLOR = new Color(231, 130, 132);
+    public static Font font;
+    public static Image img = null;
     private final Timer timer;
     private final JLabel label;
     private int seconds;
-    public static boolean showing = false;
-    public static Date date = new Date();
+    private int mouseX;
+    private int mouseY;
 
     public CountdownGUI(int time) {
-        showing = true;
         timer = new Timer(1000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -24,7 +34,7 @@ public class CountdownGUI extends JFrame {
                 label.setText("剩余 " + seconds + " 秒");
                 if (seconds == 0) {
                     timer.stop();
-                    label.setForeground(new Color(231, 130, 132));
+                    label.setForeground(TIME_DONE_COLOR);
                     label.setText("时间已到");
                 }
             }
@@ -36,40 +46,68 @@ public class CountdownGUI extends JFrame {
         int width = screenSize.width;
         int height = screenSize.height;
 
-        setSize(width / 3, height / 5);
+        setSize((int) (width / 2.5), height / 5);
         setTitle(time + " 秒倒计时");
+
+
         label = new JLabel();
-        label.setBackground(new Color(243, 231, 206));
+
+        label.setBackground(BACKGROUND);
         label.setOpaque(true);
-        label.setForeground(new Color(239, 159, 118));
+        label.setForeground(FONT_COLOR);
         label.setText("剩余 " + time + " 秒");
         label.setHorizontalAlignment(JLabel.CENTER);
         label.setVerticalAlignment(JLabel.CENTER);
-        label.setFont(new Font("Arial", Font.BOLD, getHeight() / 2));
+        label.setFont(font.deriveFont(Font.BOLD, getHeight() >> 1));
         add(label);
         //Setup End
 
 
         //Close Start
-        JButton closeButton = new JButton("单击关闭"); //创建关闭按钮
-        closeButton.setBackground(new Color(243, 231, 206));
+        JButton closeButton = new JButton("单击关闭计时器");
         closeButton.setSize(getWidth() / 5, getHeight() / 4);
-        closeButton.setFont(new Font("Arial", Font.BOLD, getHeight() / 8));
-        closeButton.setForeground(new Color(65, 69, 89));
+        closeButton.setFont(font.deriveFont(Font.BOLD, getHeight() >> 3));
+        closeButton.setForeground(CLOSE_BUTTON_TEXT_COLOR);
         closeButton.setContentAreaFilled(false);
         closeButton.setBorderPainted(false);
         closeButton.setFocusPainted(false);
+        closeButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
         closeButton.addActionListener(e -> {
             timer.stop();
-            showing = false;
             dispose();
         });
 
         JPanel panel = new JPanel();
-        panel.setBackground(new Color(243, 231, 206));
+        panel.setBackground(CLOSE_BACKGROUND);
         panel.add(closeButton);
         add(panel, BorderLayout.SOUTH);
         //Close END
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mouseX = e.getX();
+                mouseY = e.getY();
+            }
+        });
+
+        // add a mouse motion listener to update the frame location when dragged
+        addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                // get the current position of the mouse on the screen
+                int screenX = e.getXOnScreen();
+                int screenY = e.getYOnScreen();
+
+                // calculate the new position of the frame
+                int frameX = screenX - mouseX;
+                int frameY = screenY - mouseY;
+
+                // set the frame location
+                setLocation(frameX, frameY);
+            }
+        });
+        //Mouse move
 
         //Show UI
         setLocation(width / 2 - (getWidth() / 2), height / 2 - (getHeight() / 2));
@@ -78,40 +116,70 @@ public class CountdownGUI extends JFrame {
     }
 
     public static void main(String[] args) {
-        java.util.Timer show = new java.util.Timer();
-        show.schedule(new TimerTask() {
+        System.setProperty("awt.useSystemAAFontSettings", "on");
+        System.setProperty("swing.aatext", "true");
+
+        //Jar resources load
+        URL url = CountdownGUI.class.getResource("/icon.png");
+        URL fontURL = CountdownGUI.class.getResource("/SourceHanSansSC-Normal-2.otf");
+        if (url == null || fontURL == null) {
+            System.exit(0);
+            return;
+        }
+        try {
+            InputStream fontStream = fontURL.openStream();
+            InputStream input = url.openStream();
+            img = ImageIO.read(input);
+            font = Font.createFont(Font.TRUETYPE_FONT, fontStream);
+            font = font.deriveFont(Font.BOLD);
+            input.close();
+            fontStream.close();
+        } catch (IOException | FontFormatException ignored) {
+        }
+
+        setTimerAt(12, 39, 0);
+        //Add more at there
+
+        if (args.length > 0) {
+            showTimer(Integer.parseInt(args[0]));
+        }
+        //Just for some test
+    }
+
+    private static void setTimerAt(int h, int m, int s) {
+        java.util.Timer timer = new java.util.Timer();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, h);
+        calendar.set(Calendar.MINUTE, m);
+        calendar.set(Calendar.SECOND, s);
+        Date date = calendar.getTime();
+        timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 showTimer(30);
-//                if () {
-//                    showTimer(30);
-//                } else if () {
-//                    showTimer(30);
-//                } else if () {
-//                    showTimer(30);
-//                }
             }
-        }, 0, 800);
+        }, date, 1000 * 60 * 60 * 24);
     }
 
     private static void showTimer(int time) {
-        if (showing) return;
         Thread thread = new Thread(() -> {
-
             CountdownGUI frame = new CountdownGUI(time);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setResizable(false);
             frame.setAlwaysOnTop(true);
             frame.setUndecorated(true);
-            frame.setBackground(new Color(243, 231, 206));
+            frame.setBackground(BACKGROUND);
 
-            Shape shape = new RoundRectangle2D.Double(0, 0, frame.getWidth(), frame.getHeight(), 64, 64);
+            Shape shape = new RoundRectangle2D.Double(0, 0, frame.getWidth(), frame.getHeight(), 32, 32);
             frame.setShape(shape);
 
+            frame.setIconImage(img);
+            frame.setCursor(new Cursor(Cursor.MOVE_CURSOR));
+
+            //Show the frame
             frame.setVisible(true);
         });
 
-        thread.setName("Timer");
         thread.start();
     }
 }
